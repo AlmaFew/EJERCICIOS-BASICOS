69014073ac2bd6d594c6f78ed2d605983e73e83b
Andrea Few <almafew@gmail.com>
2021-03-19 00:59:18 -0300

    Áreas y perímetros de rectángulo, cuadrado y triángulo

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,39 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.Write("Ingrese el ancho del rectángulo: ");
+            float ancho;
+            ancho = float.Parse(Console.ReadLine());
+            Console.Write("Ingrese la altura del rectángulo:");
+            float altura;
+            altura = float.Parse(Console.ReadLine());
+            float area = altura * ancho;
+            float perimetro = (altura * 2) + (ancho * 2);
+            Console.WriteLine("El área del rectángulo es: " + area.ToString("f2") +
+            " y el perímetro es: " + perimetro.ToString("F2"));
+            Console.Write("Presione enter para continuar");
+            Console.ReadLine();
+            Console.Write("Ingrese la medida de un lado del cuadrado: ");
+            float lado;
+            lado = float.Parse(Console.ReadLine());
+            float areac = lado * lado;
+            float perimetroc = lado + lado + lado + lado;
+            Console.WriteLine("El área del cuadrado es: " + areac.ToString("f2") +
+            " y el perímetro es: " + perimetroc.ToString("F2"));
+            Console.Write("Presione enter para continuar");
+            Console.ReadLine();
+            Console.Write("Ingrese la base del triángulo equilátero: ");
+            float baset;
+            baset = float.Parse(Console.ReadLine());
+            Console.Write("Ingrese la altura del triángulo equilátero: ");
+            float alturat;
+            alturat = float.Parse(Console.ReadLine());
+            float areat = (baset * alturat) / 2;
+            float perimetrot = baset * 3;
+            Console.WriteLine("El área del triángulo es: " + areat.ToString("f2") +
+            " y el perímetro es: " + perimetrot.ToString("F2"));
+            Console.Write("Presione enter para salir");
+            Console.ReadLine();
         }
     }
 }
